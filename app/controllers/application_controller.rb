af7f56b51e226a53eb495de385283c705471aa47
lcymburajr <lcymburajr@ljcdesign.com>
2015-03-31 15:35:13 -0400

    sS

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,11 @@
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
+  before_filter :expire_hsts
   protect_from_forgery with: :exception
   include SessionsHelper
+  private
+  def expire_hsts
+    response.headers["Strict-Transport-Security"] = 'max-age=0'
+  end
 end
